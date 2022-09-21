adcd67d1db493720ed91f604197be5b6b49e8608
Vera <ladog-vera@mail.ru>
2022-09-21 20:07:26 +0700

    добавила задачу 8

---
+++ DZ1/Task8/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N. */
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+// count - счётчик, 2 - первое чётное число
+int count = 2; 
+
+if(num <= 1)
+{
+    Console.Write("Недействующий диапозон  ");
+}
+
+while(count <= num)
+{
+    Console.Write($"{count} ");
+    count += 2;
+}

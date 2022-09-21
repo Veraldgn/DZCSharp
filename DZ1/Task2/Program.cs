34001a4f53106b0296f2fb60f375474f6e707cbd
Vera <ladog-vera@mail.ru>
2022-09-21 17:43:13 +0700

    добавила папку с первым дз

---
+++ DZ1/Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Напишите программу, которая на вход принимает два числа и выдает, 
+какое число большее, а какое меньшее */
+
+Console.Clear();
+
+Console.WriteLine("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+
+if(num1 > num2)
+{
+    Console.WriteLine($"max = {num1}, min = {num2}");
+}
+else if(num2 > num1)
+{
+    Console.WriteLine($"max = {num2}, min = {num1}");
+}
+else
+{
+    Console.WriteLine("Введенные числа равны");
+}

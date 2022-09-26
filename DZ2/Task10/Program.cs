6bf3a7d13d30f039b1d7e1c785f1cf255420a05d
Vera <ladog-vera@mail.ru>
2022-09-26 10:57:43 +0700

    добавила папку для второго дз и задачу 10

---
+++ DZ2/Task10/Program.cs
@@ -0,0 +1,9 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное число и на выходе 
+показывает вторую цифру этого числа. */
+
+Console.Clear();
+
+Console.WriteLine("Введите  число: ");
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"num = {num / 10 % 10}");

c45286767b6ec1dec447a0b8eed777bfa743e0cb
Vera <ladog-vera@mail.ru>
2022-10-03 21:34:47 +0700

    добавила задачу 27

---
+++ DZ4/Task27/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+int result = Sum(num);
+Console.WriteLine($"Сумма цифр = {result}");
+
+int Sum(int number)
+{
+    int digit = 0;
+    while(number > 0)
+    {
+        digit += number % 10;
+        number /= 10;
+    }
+    return digit;
+}

16dca25d2060fa3f5f8a1fc974cdccbe1fe371b0
Vera <ladog-vera@mail.ru>
2022-10-06 16:36:33 +0700

    исправила ошибку

--- DZ4/Task25/Program.cs
+++ DZ4/Task25/Program.cs
@@ -3,7 +3,14 @@
 3, 5 -> 243 (3⁵)
 2, 4 -> 16 */
 
+Console.Clear();
 
+Console.Write("Введите первое число ");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine()!);
+int result = Power(num1, num2);
+Console.WriteLine(result);
 
 int Power(int a, int b)
 {
@@ -15,11 +22,4 @@ int Power(int a, int b)
     return res;
 }
 
-Console.Clear();
 
-Console.Write("Введите первое число ");
-int num1 = int.Parse(Console.ReadLine()!);
-Console.Write("Введите второе число: ");
-int num2 = int.Parse(Console.ReadLine()!);
-int result = Power(num1, num2);
-Console.WriteLine(result);

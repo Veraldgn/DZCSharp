6d8ba59ff9da20d4a273aec65b3a5ab29c9eece4
Vera <ladog-vera@mail.ru>
2022-10-13 22:59:27 +0700

    дз 6

---
+++ DZ6/Task41/Program.cs
@@ -0,0 +1,40 @@
+﻿/* Пользователь вводит с клавиатуры число M, потом вводит M чисел ЧЕРЕЗ ENTER. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+5 0 7 8 -2 -2 -> 2
+5 1 -7 567 89 223-> 3 */
+Console.Clear();
+
+Console.Write("Укажите размер массива: ");
+int arrLength = int.Parse(Console.ReadLine()!);
+int[] array = new int[arrLength];
+SettArray(array);
+PrintArray(array);
+Console.WriteLine($"Количество чисел больше 0: {PosQuan(array)}");
+
+void SettArray(int[] array)
+{
+
+    for (int i = 0; i < arrLength; i++)
+    {
+        Console.Write($"Введите {i + 1} элемент массива: ");
+        array[i] = int.Parse(Console.ReadLine()!);
+
+    }
+}
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine($"[{String.Join(",", array)}]");
+}
+
+int PosQuan(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < arrLength; i++)
+    {
+        if (array[i] > 0)
+            count++;
+
+    }
+    return count;
+}
---
+++ DZ6/Task43/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Напишите программу, которая найдёт точку пересечения двух прямых, заданных 
+уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+Console.Clear();
+
+double b1 = SettArray("Введите b1: ");
+double k1 = SettArray("Введите k1: ");
+double b2 = SettArray("Введите b2: ");
+double k2 = SettArray("Введите k2: ");
+SectLine();
+
+double SettArray(string mes)
+{
+    Console.Write(mes);
+    double num = double.Parse(Console.ReadLine()!);
+    return num;
+}
+void SectLine()
+{
+    double x = Math.Round((b2 - b1) / (k1 - k2), 2);
+    double y = Math.Round(k1 * x + b1, 2);
+    Console.WriteLine($"Точка пересечения: {x}, {y}");
+}
